e757c96a59a1f63afd6143997abe3379db60a736
AisuShimasu <59924732+AisuShimasu@users.noreply.github.com>
2021-10-07 15:52:37 +0200

    Update Menu.java

--- cpoa_td2/src/Main/Menu.java
+++ cpoa_td2/src/Main/Menu.java
@@ -1,21 +1,14 @@
-package Main;
+package main;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Iterator;
-import java.util.Set;
-
 import fabrique.DAOFactory;
 import dao.Persistance;
-import pojo.Abonnement;
-import pojo.Client;
-import pojo.Periodicite;
-import pojo.Revue;
+import metier.Abonnement;
+import metier.Client;
+import metier.Periodicite;
+import metier.Revue;
 
 public class Menu {
 	private Scanner myObj = new Scanner(System.in);
@@ -25,7 +18,7 @@ public class Menu {
 		this.obj = obj;
 	}
 	
-	public void menu() {
+	public void menu() throws Exception {
 		System.out.println("Tapez: \n1 - Table Abonnements\n2 - Table Clients\n3 - Table Periodicite\n4 - Table Revues\5 - changement de persistance");
 		String choix=myObj.nextLine();
 		if(choix.equals("1"))menuAbonnement();
@@ -35,16 +28,14 @@ public class Menu {
 		if(choix.equals("5"))changerPersistance();
 	}
 
-	private void changerPersistance() {
-		// TODO Auto-generated method stub
+	private void changerPersistance() throws Exception {
 		System.out.println("Tapez: \n1 -pour etre en mode MYSQL\n2 - pour etre en mode ListeMemoire");
 		String choix=myObj.nextLine();
 		if(choix.equals("1"))new Menu(fabrique.DAOFactory.getDAOFactory(Persistance.valueOf("MySQL"))).menu();
 		else if(choix.equals("2"))new Menu(fabrique.DAOFactory.getDAOFactory(Persistance.valueOf("LISTE_MEMOIRE"))).menu();
 	}
 
-	private void menuRevue() {
-		// TODO Auto-generated method stub
+	private void menuRevue() throws Exception {
 		System.out.println("Tapez: \n1 - Ajouter une revue\n2 - Modifier une revue\n3 - Supprimer une revue");
 		String choix = myObj.nextLine();
 		if(choix.equals("1")) {
@@ -56,15 +47,15 @@ public class Menu {
 			String Visuel = myObj.nextLine();
 			System.out.println("Id de la periodicite de la revue :");
 			int idperiodicite=myObj.nextInt();
-			Revue rev=new Revue(0,Titre,Description, 0, Visuel, obj.getPeriodiciteDAO().getByid(idPeriodicite));
-			obj.getRevueDAO().create(rev);		
+			Revue rev=new Revue(0,Titre,Description, 0, Visuel, idperiodicite);
+			obj.getRevueDAO().create(rev);
 			}
 		
 		if(choix.equals("2")) {
-			System.out.println("Entrez l'id de la revue à modifier :");
+			System.out.println("Entrez l'id de la revue Ã  modifier :");
 			int id=myObj.nextInt();
 			myObj.nextInt();
-			Revue rev=obj.getRevueDAO().getByid(id);
+			Revue rev=obj.getRevueDAO().getById(id);
 			System.out.println("Nouveau titre de la revue :");
 			String Titre=myObj.nextLine();
 			System.out.println("Nouvelle description  de la revue :");
@@ -78,16 +69,15 @@ public class Menu {
 			}
 		
 		else if(choix.equals("3")) {
-			System.out.println("Entrez l'id de la revue à supprimer");
+			System.out.println("Entrez l'id de la revue Ã  supprimer");
 			int id=myObj.nextInt();
 			myObj.nextLine();
-			obj.getRevueDao().delete(obj.getRevueDAO().getByid(id));
+			obj.getRevueDAO().delete(obj.getRevueDAO().getById(id));
 		}
 		menu();
 	}
 
-	private void menuPeriodicite() {
-		// TODO Auto-generated method stub
+	private void menuPeriodicite() throws Exception {
 		System.out.println("Tapez: \n1 - Ajouter une periodicite\n2 - Modifier une periodicite\n3 - Supprimer une periodicite");
 		String choix = myObj.nextLine();
 		if(choix.equals("1")) {
@@ -98,10 +88,10 @@ public class Menu {
 			}
 		
 		if(choix.equals("2")) {
-			System.out.println("Entrez l'id de la periodicite à modifier :");
+			System.out.println("Entrez l'id de la periodicite Ã  modifier :");
 			int id=myObj.nextInt();
 			myObj.nextInt();
-			Periodicite per=obj.getPeriodiciteDAO().getByid(id);
+			Periodicite per=obj.getPeriodiciteDAO().getById(id);
 			System.out.println("Nouveau libelle de la Periodicite :");
 			String Libelle=myObj.nextLine();
 			per.setLibelle(Libelle);
@@ -109,16 +99,15 @@ public class Menu {
 			}
 		
 		else if(choix.equals("3")) {
-			System.out.println("Entrez l'id de la periodicite à supprimer");
+			System.out.println("Entrez l'id de la periodicite Ã  supprimer");
 			int id=myObj.nextInt();
 			myObj.nextLine();
-			obj.getPeriodiciteDao().delete(obj.getPeriodiciteDAO().getByid(id));
+			obj.getPeriodiciteDAO().delete(obj.getPeriodiciteDAO().getById(id));
 		}
 		menu();
 	}
 
-	private void menuClient() {
-		// TODO Auto-generated method stub
+	private void menuClient() throws Exception {
 		System.out.println("Tapez: \n1 - Ajouter un client\n2 - Modifier un client\n3 - Supprimer un client");
 		String choix = myObj.nextLine();
 		if(choix.equals("1")) {
@@ -126,7 +115,7 @@ public class Menu {
 			String Nom = myObj.nextLine();
 			System.out.println("Prenom du client :");
 			String Prenom = myObj.nextLine();
-			System.out.println("N° de rue du client :");
+			System.out.println("NÂ° de rue du client :");
 			String Rue = myObj.nextLine();
 			System.out.println("Voie de la rue du client :");
 			String Voie = myObj.nextLine();
@@ -136,20 +125,20 @@ public class Menu {
 			String Ville = myObj.nextLine();
 			System.out.println("Pays du client :");
 			String Pays = myObj.nextLine();
-			Client cli=new Client (0,Nom,Prenom,Rue,Voie,Postal,Ville,Pays);
+			Client cli=new Client (Nom,0,Prenom,Rue,Voie,Postal,Ville,Pays);
 			obj.getClientDAO().create(cli);
 			}
 		
 		if(choix.equals("2")) {
-			System.out.println("Entrez l'id du client à modifier :");
+			System.out.println("Entrez l'id du client Ã  modifier :");
 			int id=myObj.nextInt();
 			myObj.nextInt();
-			Client cli=obj.getClientDAO().getByid(id);
+			Client cli=obj.getClientDAO().getById(id);
 			System.out.println("Nouveau nom du client :");
 			String Nom=myObj.nextLine();
 			System.out.println("Nouveau prenom du client :");
 			String Prenom=myObj.nextLine();
-			System.out.println("Nouveau N° de rue du client :");
+			System.out.println("Nouveau NÂ° de rue du client :");
 			String Rue=myObj.nextLine();
 			System.out.println("Nouvelle voie du client :");
 			String Voie=myObj.nextLine();
@@ -170,66 +159,69 @@ public class Menu {
 			}
 		
 		else if(choix.equals("3")) {
-			System.out.println("Entrez l'id du client à supprimer");
+			System.out.println("Entrez l'id du client Ã  supprimer");
 			int id=myObj.nextInt();
 			myObj.nextLine();
-			obj.getClientDao().delete(obj.getClientDAO().getByid(id));
+			obj.getClientDAO().delete(obj.getClientDAO().getById(id));
 		}
 		menu();
 	}
 
-	private void menuAbonnement() {
-		// TODO Auto-generated method stub
+	private void menuAbonnement() throws Exception {
 		System.out.println("Tapez: \n1 - Ajouter un abonnement\n2 - Modifier un abonnement\n3 - Supprimer un abonnement");
 		String choix = myObj.nextLine();
 		if(choix.equals("1")) {
-			System.out.println("Debut de l'abonnement au format (YYYY-MM-DD hh:mm:ss) :");
+			System.out.println("Debut de l'abonnement au format (YYYY-MM-DD) : ");
 			String Debut = myObj.nextLine();
-			DateTimeFormatter Formatage1 = DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm:ss");
-			System.out.println("Fin de l'abonnementt au format (YYYY-MM-DD hh:mm:ss):");
+			DateTimeFormatter formatageDebut = DateTimeFormatter.ofPattern("YYYY-MM-DD");
+			LocalDate dateDebut=null;
+			dateDebut = LocalDate.parse(Debut, formatageDebut);
+			System.out.println("Fin de l'abonnementt au format (YYYY-MM-DD) : ");
 			String Fin = myObj.nextLine();
-			DateTimeFormatter Formatage2 = DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm:ss");
+			DateTimeFormatter formatageFin = DateTimeFormatter.ofPattern("YYYY-MM-DD");
+			LocalDate dateFin=null;
+			dateFin = LocalDate.parse(Fin, formatageFin);
 			System.out.println("Id du client :");
 			int idclient=myObj.nextInt();
 			System.out.println("Id de ma revue :");
 			int idrevue=myObj.nextInt();
-			Abonnement abo=new Abonnement (0,Debut,Fin,obj.getClientDAO().getByid(idclient), obj.getRevueDAO().getbyId(idrevue));
+			Abonnement abo=new Abonnement (0, dateDebut, dateFin, idclient, idrevue);
 			obj.getAbonnementDAO().create(abo);
 			}			
 
 		if(choix.equals("2")) {
-			System.out.println("Entrez l'id de l'abonnement à modifier :");
+			System.out.println("Entrez l'id de l'abonnement Ã  modifier :");
 			int id=myObj.nextInt();
 			myObj.nextInt();
-			Abonnement abo=obj.getAbonnementDAO().getByid(id);
-			System.out.println("Nouveau Debut de l'abonnement au format (YYYY-MM-DD hh:mm:ss) :");
+			Abonnement abo=obj.getAbonnementDAO().getById(id);
+			System.out.println("Nouveau Debut de l'abonnement au format (YYYY-MM-DD) :");
 			String Debut=myObj.nextLine();
-			DateTimeFormatter Formatage1 = DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm:ss");
-			LocalDateTime Date1=null;
-			Date1 = LocalDateTime.parse(Debut, Formatage1);
-			System.out.println("Nouvelle Fin de l'abonnement au format (YYYY-MM-DD hh:mm:ss) :");
+			DateTimeFormatter formatageDebut = DateTimeFormatter.ofPattern("YYYY-MM-DD");
+			LocalDate dateDebut=null;
+			dateDebut = LocalDate.parse(Debut, formatageDebut);
+			System.out.println("Nouvelle Fin de l'abonnement au format (YYYY-MM-DD) :");
 			String Fin=myObj.nextLine();
-			DateTimeFormatter Formatage2 = DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm:ss");
-			LocalDateTime Date2=null;
-			Date2 = LocalDateTime.parse(Fin, Formatage2);
+			DateTimeFormatter formatageFin = DateTimeFormatter.ofPattern("YYYY-MM-DD");
+			LocalDate dateFin=null;
+			dateFin = LocalDate.parse(Fin, formatageFin);
 			System.out.println("Nouveau Id du client :");
 			int idclient=myObj.nextInt();
 			myObj.nextLine();
 			System.out.println("Nouvelle Id de la revue :");
 			int idrevue=myObj.nextInt();
 			myObj.nextLine();
-			abo.setdate_debut(Debut);
-			abo.setdate_fin(Fin);
-			abo.setid_client(idclient);
-			abo.setid_revue(idrevue);
+			abo.setDate_debut(dateDebut);
+			abo.setDate_fin(dateFin);
+			abo.setId_client(idclient);
+			abo.setId_revue(idrevue);
 			obj.getAbonnementDAO().update(abo);
 			}
 		
 		else if(choix.equals("3")) {
-			System.out.println("Entrez l'id de l'abonnement à supprimer");
+			System.out.println("Entrez l'id de l'abonnement Ã  supprimer");
 			int id=myObj.nextInt();
 			myObj.nextLine();
-			obj.getAbonnementDao().delete(obj.getAbonnementDAO().getByid(id));
+			obj.getAbonnementDAO().delete(obj.getAbonnementDAO().getById(id));
 		}
 		menu();
 	}
